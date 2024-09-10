5bc220c20ec31294e698f07b5f5f17665fcba094
Kishan <kramolar@gmail.com>
2024-09-10 19:00:54 -0400

    phone number has link

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -4,7 +4,9 @@ export const BottomBar = () => {
   return (
     <div className="fixed bottom-2 items-center flex justify-center py-1 animate-bounce w-full bg-brown-shade2 md:hidden">
       <img src={phone} className="h-6 w-6" />
-      <div className="py-1 text-grey-shade">+1 (647) 806 - 2995</div>
+      <a href="tel:+16478062995" className="py-1 text-grey-shade">
+        +1 (647) 806 - 2995
+      </a>
     </div>
   );
 };
